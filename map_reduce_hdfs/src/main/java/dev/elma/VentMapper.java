d29bd08c634a2959999b7060b8586dbf7f318ea1
Elma-dev <67378945+Elma-dev@users.noreply.github.com>
2023-10-20 11:33:04 +0100

    commit

--- map_reduce_hdfs/src/main/java/dev/elma/VentMapper.java
+++ map_reduce_hdfs/src/main/java/dev/elma/VentMapper.java
@@ -1,2 +1,20 @@
-package dev.elma;public class VentMapper {
+package dev.elma;
+
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+public class VentMapper extends Mapper<IntWritable, Text,Text, DoubleWritable> {
+    @Override
+    protected void map(IntWritable key, Text value, Mapper<IntWritable, Text, Text, DoubleWritable>.Context context) throws IOException, InterruptedException {
+        // date ville produit prix
+        String[] row = value.toString().split(",");
+        String city=row[1];
+        Double price=Double.parseDouble(row[3]);
+        context.write(new Text(city),new DoubleWritable(price));
+    }
 }
--- map_reduce_hdfs/src/main/java/dev/elma/VentReduce.java
+++ map_reduce_hdfs/src/main/java/dev/elma/VentReduce.java
@@ -1,2 +1,22 @@
-package dev.elma;public class VentReduce {
+package dev.elma;
+
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+import java.util.Iterator;
+
+public class VentReduce extends Reducer<Text, DoubleWritable,Text, DoubleWritable> {
+    @Override
+    protected void reduce(Text key, Iterable<DoubleWritable> values, Reducer<Text, DoubleWritable, Text, DoubleWritable>.Context context) throws IOException, InterruptedException {
+        double p=0;
+        Iterator<DoubleWritable> iterator = values.iterator();
+        while (iterator.hasNext()){
+           p+= iterator.next().get();
+        }
+        context.write(key,new DoubleWritable(p));
+    }
 }
