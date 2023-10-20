b3cabfb7396a49a673ee61b770e639338186c7aa
Elma-dev <67378945+Elma-dev@users.noreply.github.com>
2023-10-20 09:26:26 +0100

    map reduce commit

--- src/main/java/dev/elma/Main.java
+++ src/main/java/dev/elma/Main.java
@@ -1,9 +1,37 @@
-package org.example;
+package dev.elma;
+
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.hdfs.HdfsConfiguration;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+        Configuration configuration=new Configuration();
+        configuration.set("fs.defaultFS", "hdfs://localhost:8021");
+        FileSystem fileSystem=FileSystem.get(configuration);
+        Path path=new Path("SDIA/PYTHON/Cours/hello.txt");
+
+        FSDataInputStream dataInputStream = fileSystem.open(path);
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(dataInputStream, StandardCharsets.UTF_8));
+        String row=null;
+
+        while ((row=bufferedReader.readLine())!=null){
+            System.out.println(row);
+        }
+        dataInputStream.close();
+        fileSystem.close();
+
     }
 }
